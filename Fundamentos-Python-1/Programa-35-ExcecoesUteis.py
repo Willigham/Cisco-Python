071bb51deb78bdeb4fcd93ad8d35067e892bbb6a
Willigham Coelho <willigamcoelho@gmail.com>
2025-10-06 12:19:38 -0300

    Comit 4 - Modulo 4 Completo

---
+++ Fundamentos-Python-1/Programa-35-ExcecoesUteis.py
@@ -0,0 +1,9 @@
+# ZeroDivisionError
+def dividir(a, b):
+    return a / b
+# ValueError
+def converter_para_inteiro(valor):
+    return int(valor)
+# TypeError
+def concatenar_strings(a, b):
+    return a + b
